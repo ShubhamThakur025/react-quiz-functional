13c08bc41e262583b9e64bedeeea75f6b92ed58a
ShubhamThakur025 <thakur0805@gmail.com>
2024-01-28 15:19:42 +0530

    Bug fized

--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -4,16 +4,30 @@ export default class ResultComponent extends Component {
     constructor(props) {
         super(props)
     }
-
+    remarks = (percent) => {
+        switch (true) {
+            case percent <= 30:
+                return "You Need Practice!"
+            case percent <= 60:
+                return "You did good!"
+            case percent <= 90:
+                return "Almost there!"
+            case percent > 90:
+                return "Perfecto!"
+        }
+    }
     render() {
+        const percentage = (this.props.score / 5) * 100
+        let attempted = this.props.attempted
+        if (attempted < 0) { attempted = 0 }
         return (
             <div className='result-card'>
                 <h1 className='text-center title-prime' style={{ color: "green" }}>Result</h1>
                 <div className="result">
 
                     <div className="status">
-                        <h3 className="text-center top-heading">You need more practice!</h3>
-                        <h1 className='text-center' style={{ color: "blue" }}>Your Score is 20%</h1>
+                        <h3 className="text-center top-heading">{this.remarks(percentage)}</h3>
+                        <h1 className='text-center' style={{ color: "blue" }}>{`Your score is ${percentage}%`}</h1>
 
                         <table id='stats-table'>
                             <tbody>
@@ -23,7 +37,7 @@ export default class ResultComponent extends Component {
                                 </tr>
                                 <tr>
                                     <td>Number of attempted questions</td>
-                                    <td className='text-right'>{this.props.attempted}</td>
+                                    <td className='text-right'>{attempted}</td>
                                 </tr>
                                 <tr>
                                     <td>Number of corrected questions</td>
@@ -32,7 +46,7 @@ export default class ResultComponent extends Component {
 
                                 <tr>
                                     <td>Number of wrong questions</td>
-                                    <td className='text-right'>{this.props.attempted - this.props.score}</td>
+                                    <td className='text-right'>{attempted - this.props.score}</td>
                                 </tr>
                             </tbody>
                         </table>
